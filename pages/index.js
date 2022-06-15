124f038a5fc254c4cf7d05d3ee126992b31a1449
FellowMustard <rachmawanng33@gmail.com>
2022-06-15 16:57:08 +0700

    Piano animation fixing and chords

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import { MdOutlinePiano } from "react-icons/md";
 import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [pianoActive, setPianoActive] = useState(false);
+  const pianoClick = () => {
+    setPianoActive(true);
+  };
+  const pianoLeave = () => {
+    setPianoActive(false);
+  };
+  const pianoKeysReact = (keys, leave = false) => {
+    if (pianoActive) {
+      console.log(keys);
+      if (leave) {
+        console.log(keys + " leave");
+      }
+    }
+  };
   return (
     <main className={styles.mainContainer}>
       <nav className={styles.pageNav}>
@@ -11,19 +27,81 @@ export default function Home() {
         </div>
       </nav>
       <section className={styles.pianoSection}>
-        <div className={styles.piano}>
-          <div className={styles.pianoWhite}></div>
+        <div
+          className={styles.piano}
+          onClick={() => pianoClick()}
+          onMouseDown={() => pianoClick()}
+          onMouseUp={() => pianoLeave()}
+          onMouseLeave={() => pianoLeave()}
+        >
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("C")}
+              onMouseLeave={() => pianoKeysReact("C", true)}
+            >
+              C
+            </span>
+          </div>
           <div className={styles.pianoBlack}></div>
-          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("D")}
+              onMouseLeave={() => pianoKeysReact("D", true)}
+            >
+              D
+            </span>
+          </div>
           <div className={styles.pianoBlack}></div>
-          <div className={styles.pianoWhite}></div>
-          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("E")}
+              onMouseLeave={() => pianoKeysReact("E", true)}
+            >
+              E
+            </span>
+          </div>
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("F")}
+              onMouseLeave={() => pianoKeysReact("F", true)}
+            >
+              F
+            </span>
+          </div>
           <div className={styles.pianoBlack}></div>
-          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("G")}
+              onMouseLeave={() => pianoKeysReact("G", true)}
+            >
+              G
+            </span>
+          </div>
           <div className={styles.pianoBlack}></div>
-          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("A")}
+              onMouseLeave={() => pianoKeysReact("A", true)}
+            >
+              A
+            </span>
+          </div>
           <div className={styles.pianoBlack}></div>
-          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoWhite}>
+            <span
+              className={styles.pianoKeysName}
+              onMouseEnter={() => pianoKeysReact("B")}
+              onMouseLeave={() => pianoKeysReact("B", true)}
+            >
+              B
+            </span>
+          </div>
         </div>
       </section>
     </main>
