fa458e590840ffb51dabe61bbd0ebaf17a7c0895
FellowMustard <rachmawanng33@gmail.com>
2022-06-15 13:56:37 +0700

    Add piano and animation

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import Head from "next/head";
-import Image from "next/image";
+import { MdOutlinePiano } from "react-icons/md";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <main className={styles.mainContainer}>
       <nav className={styles.pageNav}>
-        <div className={styles.logoNav}>Zeephiano</div>
+        <div className={styles.logoNav}>
+          <MdOutlinePiano className={styles.logoPiano} />
+          <span>Zeephiano</span>
+        </div>
       </nav>
+      <section className={styles.pianoSection}>
+        <div className={styles.piano}>
+          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoBlack}></div>
+          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoBlack}></div>
+          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoBlack}></div>
+          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoBlack}></div>
+          <div className={styles.pianoWhite}></div>
+          <div className={styles.pianoBlack}></div>
+          <div className={styles.pianoWhite}></div>
+        </div>
+      </section>
     </main>
   );
 }
